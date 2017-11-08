a719885288cef18f5e5b755aa228a4841a654e31
郭超 <guochao_ios@126.com>
2017-11-08 16:11:40 +0800

    Initial Commit

---
+++ TestSizeX/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestSizeX
+//
+//  Created by 郭超 on 2017/11/8.
+//  Copyright © 2017年 郭超. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
