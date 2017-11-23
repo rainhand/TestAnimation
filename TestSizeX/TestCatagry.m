0a795a6ccd747433c9e93a2052db709fb8e91275
郭超 <guochao_ios@126.com>
2017-11-23 12:34:04 +0800

    fix bug

---
+++ TestSizeX/TestCatagry.m
@@ -0,0 +1,21 @@
+//
+//  TestCatagry.m
+//  TestSizeX
+//
+//  Created by 郭超 on 2017/11/23.
+//  Copyright © 2017年 郭超. All rights reserved.
+//
+
+#import "TestCatagry.h"
+
+@implementation TestCatagry
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
